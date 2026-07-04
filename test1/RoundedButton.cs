e27d70eee5532aa3bfbee9ae8059938cedf71975
Aneesa <@Aneesa-5>
2026-07-04 23:36:46 +0800

    interface

---
+++ test1/RoundedButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace test1
+{
+    public class RoundedButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            int radius = 45;
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(Width - radius, Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            Region = new Region(path);
+
+            base.OnPaint(pevent);
+        }
+    }
+}
