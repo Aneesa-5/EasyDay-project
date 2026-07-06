f771a8addf137841734290ab0fa0d48c62bb92f7
Aliff Ikhwan <Aliffikhwan502@gmail.com>
2026-07-07 07:42:29 +0800

    oh yeah, forgot abt comment

---
+++ test1/Translation.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace EasyDay_SignUp
+{
+    partial class Translation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Translation));
+            btnTranslate = new Guna.UI2.WinForms.Guna2Button();
+            cmbSourceLanguage = new Guna.UI2.WinForms.Guna2ComboBox();
+            textInput = new Guna.UI2.WinForms.Guna2TextBox();
+            textOutput = new Guna.UI2.WinForms.Guna2TextBox();
+            cmbTargetLanguage = new Guna.UI2.WinForms.Guna2ComboBox();
+            SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            btnTranslate.CustomizableEdges = customizableEdges1;
+            btnTranslate.DisabledState.BorderColor = Color.DarkGray;
+            btnTranslate.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnTranslate.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnTranslate.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnTranslate.FillColor = Color.Goldenrod;
+            btnTranslate.FocusedColor = Color.Black;
+            btnTranslate.Font = new Font("Lucida Bright", 10.8F, FontStyle.Italic);
+            btnTranslate.ForeColor = Color.FromArgb(128, 64, 64);
+            btnTranslate.Location = new Point(251, 317);
+            btnTranslate.Name = "btnTranslate";
+            btnTranslate.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnTranslate.Size = new Size(272, 39);
+            btnTranslate.TabIndex = 0;
+            btnTranslate.Text = "TRANSLATE";
+            btnTranslate.Click += btnTranslate_Click;
+            // 
+            // cmbSourceLanguage
+            // 
+            cmbSourceLanguage.BackColor = Color.Transparent;
+            cmbSourceLanguage.CustomizableEdges = customizableEdges3;
+            cmbSourceLanguage.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbSourceLanguage.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSourceLanguage.FillColor = Color.RoyalBlue;
+            cmbSourceLanguage.FocusedColor = Color.FromArgb(94, 148, 255);
+            cmbSourceLanguage.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            cmbSourceLanguage.Font = new Font("Lucida Bright", 10.8F, FontStyle.Italic);
+            cmbSourceLanguage.ForeColor = Color.FromArgb(68, 88, 112);
+            cmbSourceLanguage.ItemHeight = 30;
+            cmbSourceLanguage.Location = new Point(50, 235);
+            cmbSourceLanguage.Name = "cmbSourceLanguage";
+            cmbSourceLanguage.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            cmbSourceLanguage.Size = new Size(179, 36);
+            cmbSourceLanguage.TabIndex = 1;
+            // 
+            // textInput
+            // 
+            textInput.CustomizableEdges = customizableEdges5;
+            textInput.DefaultText = "Type in here...";
+            textInput.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            textInput.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            textInput.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            textInput.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            textInput.FillColor = Color.Snow;
+            textInput.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            textInput.Font = new Font("Lucida Bright", 10.8F, FontStyle.Italic);
+            textInput.ForeColor = Color.FromArgb(64, 64, 64);
+            textInput.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            textInput.Location = new Point(13, 13);
+            textInput.Margin = new Padding(4, 4, 4, 4);
+            textInput.Multiline = true;
+            textInput.Name = "textInput";
+            textInput.PlaceholderText = "";
+            textInput.SelectedText = "";
+            textInput.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            textInput.Size = new Size(308, 201);
+            textInput.TabIndex = 2;
+            // 
+            // textOutput
+            // 
+            textOutput.CustomizableEdges = customizableEdges7;
+            textOutput.DefaultText = "Translated text...";
+            textOutput.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            textOutput.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            textOutput.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            textOutput.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            textOutput.FillColor = Color.Snow;
+            textOutput.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            textOutput.Font = new Font("Lucida Bright", 10.8F, FontStyle.Italic);
+            textOutput.ForeColor = Color.FromArgb(64, 64, 64);
+            textOutput.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            textOutput.Location = new Point(461, 13);
+            textOutput.Margin = new Padding(4, 4, 4, 4);
+            textOutput.Multiline = true;
+            textOutput.Name = "textOutput";
+            textOutput.PlaceholderText = "";
+            textOutput.SelectedText = "";
+            textOutput.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            textOutput.Size = new Size(308, 201);
+            textOutput.TabIndex = 3;
+            textOutput.TextChanged += this.textOutput_TextChanged;
+            // 
+            // cmbTargetLanguage
+            // 
+            cmbTargetLanguage.BackColor = Color.Transparent;
+            cmbTargetLanguage.CustomizableEdges = customizableEdges9;
+            cmbTargetLanguage.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbTargetLanguage.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbTargetLanguage.FillColor = Color.RoyalBlue;
+            cmbTargetLanguage.FocusedColor = Color.FromArgb(94, 148, 255);
+            cmbTargetLanguage.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            cmbTargetLanguage.Font = new Font("Lucida Bright", 10.8F, FontStyle.Italic);
+            cmbTargetLanguage.ForeColor = Color.FromArgb(68, 88, 112);
+            cmbTargetLanguage.ItemHeight = 30;
+            cmbTargetLanguage.Location = new Point(555, 235);
+            cmbTargetLanguage.Name = "cmbTargetLanguage";
+            cmbTargetLanguage.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            cmbTargetLanguage.Size = new Size(179, 36);
+            cmbTargetLanguage.TabIndex = 4;
+            // 
+            // Translation
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(782, 453);
+            Controls.Add(cmbTargetLanguage);
+            Controls.Add(textOutput);
+            Controls.Add(textInput);
+            Controls.Add(cmbSourceLanguage);
+            Controls.Add(btnTranslate);
+            Name = "Translation";
+            Text = "Form2";
+            Load += Translation_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnTranslate;
+        private Guna.UI2.WinForms.Guna2ComboBox cmbSourceLanguage;
+        private Guna.UI2.WinForms.Guna2TextBox textInput;
+        private Guna.UI2.WinForms.Guna2TextBox textOutput;
+        private Guna.UI2.WinForms.Guna2ComboBox cmbTargetLanguage;
+    }
+}
---
+++ test1/Translation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace EasyDay_SignUp
+{
+    public partial class Translation : Form
+    {
+        private const string baseUrl = "http://api.mymemory.translated.net";
+        private HttpClient httpClient;
+
+        public Translation()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+        }
+
+        private void Translation_Load(object sender, EventArgs e)
+        {
+            //cmbSourceLanguage.BorderRadius = 20;
+            //cmbTargetLanguage.BorderRadius = 20;
+            //textInput.BorderRadius = 20;
+            //textOutput.BorderRadius = 20;
+            cmbSourceLanguage.Items.AddRange(new string[] { "EN", "MS", "ID", "BN", "NE", "VI", "MY" });
+            cmbTargetLanguage.Items.AddRange(new string[] { "EN", "MS", "ID", "BN", "NE", "VI", "MY" });
+            cmbSourceLanguage.SelectedIndex = 0;
+            cmbTargetLanguage.SelectedIndex = 1;
+        }
+
+        private async Task<string> TranslateAsync(string text, string sourceLang, string targetLang)
+        {
+            string url = $"{baseUrl}/get?q={Uri.EscapeDataString(text)}&langpair={sourceLang}|{targetLang}";
+            //MessageBox.Show(url);
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseJson = await response.Content.ReadAsStringAsync();
+            //MessageBox.Show(responseJson);
+
+            var translationResult = JsonConvert.DeserializeObject<TranslationResponse>(responseJson);
+
+            if (translationResult.ResponseStatus == 200)
+            {
+                return translationResult.TranslatedText;
+            }
+
+            return string.Empty;
+        }
+
+        private void textOutput_TextChanged(object sender, EventArgs e)
+        {
+            // Leave empty if you don't need it.
+        }
+
+        private async void btnTranslate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string translatedText = await TranslateAsync(textInput.Text, cmbSourceLanguage.SelectedItem.ToString().ToLower(), cmbTargetLanguage.SelectedItem.ToString().ToLower());
+                textOutput.Text = translatedText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Translation failed. Please try again. Error: \r\n" + ex);
+                //throw;
+            }
+        }
+    }
+
+    public class TranslationResponse
+    {
+        [JsonProperty("responseStatus")]
+        public int ResponseStatus { get; set; }
+
+        [JsonProperty("responseData")]
+        public TranslationData ResponseData { get; set; }
+
+        public string TranslatedText => ResponseData?.TranslatedText;
+    }
+
+    public class TranslationData
+    {
+        [JsonProperty("translatedText")]
+        public string TranslatedText { get; set; }
+    }
+}
